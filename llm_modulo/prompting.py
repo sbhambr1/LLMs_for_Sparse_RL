7efd9c76c5214ec21f9e6a87792394d7f9b28baa
Siddhant <sbhambr1@asu.edu>
2024-04-28 19:28:59 -0700

    class abstraction for minigrid env prompt construction, TODO: add prompts for other 3

---
+++ llm_modulo/prompting.py
@@ -0,0 +1,27 @@
+from llm_modulo.env_prompts import *
+
+class MinigridPromptConstructor:
+    
+    def __init__(self, env, seed):
+        if env == 'MiniGrid-DoorKey-5x5-v0':
+            self.prompt_constructor = DoorKey5x5Prompts(env, seed)
+        elif env == 'MiniGrid-Empty-Random-5x5-v0':
+            self.prompt_constructor = EmptyRandom5x5Prompts(env, seed)
+        elif env == 'MiniGrid-LavaGapS5-v0':
+            self.prompt_constructor = LavaGapS5Prompts(env, seed)
+        elif env == 'MiniGrid-KeyCorridorS3R1-v0':
+            self.prompt_constructor = KeyCorridorS3R1Prompts(env, seed)
+        else:
+            raise NotImplementedError 
+        
+    def get_step_prompt(self, obs, add_text_desc):
+        return self.prompt_constructor.get_step_prompt(obs, add_text_desc)
+    
+    def get_prompt_with_backprompt(self, obs, backprompt, tried_actions, give_tried_actions=True):
+        return self.prompt_constructor.get_prompt_with_backprompt(obs, backprompt, tried_actions, give_tried_actions)
+
+    def convert_obs_to_text(self, observation):
+        return self.prompt_constructor.convert_obs_to_text(observation)
+    
+    def convert_obs_to_grid_text(self, observation):
+        return self.prompt_constructor.convert_obs_to_grid_text(observation)
--- scripts/llm_modulo_policy.py
+++ scripts/llm_modulo_policy.py
@@ -6,6 +6,7 @@ import gymnasium as gym
 from minigrid.wrappers import SymbolicObsWrapper
 from utils.conversation import Conversation
 from llm_modulo.backprompting import *
+from llm_modulo.prompting import *
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -19,181 +20,31 @@ parser.add_argument("--add_text_desc", default=True, help="Whether to give addit
 parser.add_argument("--give_feasible_actions", default=True, help="Whether to give feasible actions in backprompt")
 parser.add_argument("--give_tried_actions", default=True, help="Whether to give tried actions in backprompt")
 parser.add_argument("--additional_expt_info", default="", help="Additional information for experiment")
-
-ACTION_DICT = {
-    0: 'left', 
-    1: 'right', 
-    2: 'forward', 
-    3: 'pickup key', 
-    4: 'drop', 
-    5: 'open door', 
-    6: 'done'}
-OBJECT_TO_IDX = {
-    "unseen": -1,
-    "empty": 1,
-    "wall": 2,
-    "floor": 3,
-    "door": 4,
-    "key": 5,
-    "ball": 6,
-    "box": 7,
-    "goal": 8,
-    "lava": 9,
-    "agent": 10,
-}
-DIRECTION_DICT = {
-    'right': 0,
-    'down': 1,
-    'left': 2,
-    'up': 3}
-NUM_AGENT_STEPS = 30
-
-def get_initial_prompt(observation):
-        TASK_DESC = "You are tasked with solving a 3x3 maze where you will encounter objects like a key and a door along with walls. Your task is 'use the key to open the door and then get to the goal'. You can be facing in any of the four directions. To move in any direction, to pick up the key, and to open the door, you need to face in the correct direction. You will be given a description of the maze at every step and you need to choose the next action to take. The available actions are 'turn left', 'turn right', 'move forward', 'pickup key', 'open door'.\n"
-        
-        OBS_DESC = convert_obs_to_grid_text(observation)
-        
-        QUERY_DESC = "What is the next action that you should take? Only choose from the list of available actions. Do not include anything else in your response. For example, if you choose 'move forward', then only write 'move forward' in your response.\n"
-        initial_prompt = f"{TASK_DESC}\n{OBS_DESC}\n{QUERY_DESC}\n"
-        
-        return initial_prompt
+parser.add_argument("--num_agent_steps", type=int, default=30, help="Number of steps the agent can take")
+parser.add_argument("--num_backprompt_steps", type=int, default=10, help="Number of backprompts that can be given")
     
-def get_step_prompt(obs, add_text_desc):
-    TASK_DESC = "You are tasked with solving a 3x3 maze where you will encounter objects like a key and a door along with walls. Your task is 'use the key to open the door and then get to the goal'. You can be facing in any of the four directions. To move in any direction, to pick up the key, and to open the door, you need to face in the correct direction. You will be given a description of the maze at every step and you need to choose the next action to take. The available actions are 'turn left', 'turn right', 'move forward', 'pickup key', 'open door'.\n"
-    OBS_DESC = convert_obs_to_grid_text(obs)
-    QUERY_DESC = "What is the next action that the agent should take? Only choose from the list of available actions. Do not include anything else in your response. For example, if you choose 'move forward', then only write 'move forward' in your response."
-    if add_text_desc != '':
-        step_prompt = f"{TASK_DESC}\n{add_text_desc}\n{OBS_DESC}\n{QUERY_DESC}\n"
-    else:
-        step_prompt = f"{TASK_DESC}\n{OBS_DESC}\n{QUERY_DESC}\n"
-    return step_prompt
-
-def get_prompt_with_backprompt(obs, backprompt, tried_actions, give_tried_actions=True):
-    TASK_DESC = "You are tasked with solving a 3x3 maze where you will encounter objects like a key and a door along with walls. Your task is 'use the key to open the door and then get to the goal'. You can be facing in any of the four directions. To move in any direction, to pick up the key, and to open the door, you need to face in the correct direction. You will be given a description of the maze at every step and you need to choose the next action to take. The available actions are 'turn left', 'turn right', 'move forward', 'pickup key', 'open door'.\n"
-    OBS_DESC = convert_obs_to_grid_text(obs)
-    QUERY_DESC = "What is the next action that the agent should take? Only choose from the list of available actions. Do not include anything else in your response. For example, if you choose 'move forward', then only write 'move forward' in your response."
-    RETRY = "You have already tried the following actions: " + ', '.join(tried_actions) + ". Please choose another action."
-    if give_tried_actions:
-        prompt_with_backprompt = f"{TASK_DESC}\n{backprompt}\n{OBS_DESC}\n{QUERY_DESC}\n{RETRY}\n"
-    else:
-        prompt_with_backprompt = f"{TASK_DESC}\n{backprompt}\n{OBS_DESC}\n{QUERY_DESC}\n"
-    return prompt_with_backprompt
-
-def _convert_pos_to_3x3(pos):
-    return (pos[0]-1, pos[1]-1)
-
-def convert_obs_to_text(observation):
-    # Convert observation to text
-    # this has bugs due to the way the observation is structured (rows and cols are inverse in the observation array)
-    obs_array = observation['image']
-    
-    walls = []
-    unseen = []
-    for i in range(1,4):
-        for j in range(1,4):
-            if obs_array[i][j][2] == OBJECT_TO_IDX['agent']:
-                x, y = obs_array[i][j][0], obs_array[i][j][1]
-                agent_pos = _convert_pos_to_3x3((x, y))
-            if obs_array[i][j][2] == OBJECT_TO_IDX['door']:
-                x, y = obs_array[i][j][0], obs_array[i][j][1]
-                door_pos = _convert_pos_to_3x3((x, y))
-            if obs_array[i][j][2] == OBJECT_TO_IDX['key']:
-                x, y = obs_array[i][j][0], obs_array[i][j][1]
-                key_pos = _convert_pos_to_3x3((x, y))
-            if obs_array[i][j][2] == OBJECT_TO_IDX['goal']:
-                x, y = obs_array[i][j][0], obs_array[i][j][1]
-                goal_pos = _convert_pos_to_3x3((x, y))
-            if obs_array[i][j][2] == OBJECT_TO_IDX['wall']:
-                x, y = obs_array[i][j][0], obs_array[i][j][1]
-                walls.append(_convert_pos_to_3x3((x, y)))
-            if obs_array[i][j][2] == OBJECT_TO_IDX['unseen']:
-                x, y = obs_array[i][j][0], obs_array[i][j][1]
-                unseen.append(_convert_pos_to_3x3((x, y)))
-    
-    AGENT_DESC = f"In the 3x3 grid with (0, 0) at top-left and (2,2) at bottom right, you are at position {agent_pos}."
-    KEY_DESC = f"The key is at position {key_pos}."
-    DOOR_DESC = f"The door is at position {door_pos}."
-    GOAL_DESC = f"The goal is at position {goal_pos}."
-    MISC_DESC = f"There are walls at positions {walls}. There are unseen areas at positions {unseen}."
-    OBS_DESC = f"The current observation is: \n{AGENT_DESC}\n{KEY_DESC}\n{DOOR_DESC}\n{GOAL_DESC}\n{MISC_DESC}\n"
-    return OBS_DESC
-
-def convert_obs_to_grid_text(observation):
-    obs_array = observation['image']
-    agent_dir = observation['direction']
-    agent_dir = list(DIRECTION_DICT.keys())[list(DIRECTION_DICT.values()).index(agent_dir)]
-    col0, col1, col2 = [], [], []
-    for i in range(3):
-        # append key from OBJECT_TO_IDX    
-        col0.append(obs_array[1][1:4][i][2])
-        col1.append(obs_array[2][1:4][i][2])
-        col2.append(obs_array[3][1:4][i][2])
-        
-     # transpose numpy array
-    obs_matrix = np.array([col0, col1, col2])
-    obs_matrix = np.transpose(obs_matrix)
-    row0, row1, row2 = list(obs_matrix[0]), list(obs_matrix[1]), list(obs_matrix[2])
-    
-    for i in range(3):
-        row0[i] = list(OBJECT_TO_IDX.keys())[list(OBJECT_TO_IDX.values()).index(row0[i])]
-        row1[i] = list(OBJECT_TO_IDX.keys())[list(OBJECT_TO_IDX.values()).index(row1[i])]
-        row2[i] = list(OBJECT_TO_IDX.keys())[list(OBJECT_TO_IDX.values()).index(row2[i])]
-            
-    GRID_HEADER = "The current maze looks like this:\n"
-    grid_text = f"{' '.join(row0)}\n{' '.join(row1)}\n{' '.join(row2)}\n"
-    AGENT_DIR = f"You (agent) are currently facing {agent_dir}.\n"
-    
-    # TODO: make obs more descriptive
-    # Additional information if agent has picked up the key or opened the door
-    if 'key' not in row0 and 'key' not in row1 and 'key' not in row2:
-        ADDITIONAL_INFO = "You have already picked up the key.\n"
-        AGENT_DIR += ADDITIONAL_INFO
-    
-    text_obs = f"{GRID_HEADER}\n{grid_text}\n{AGENT_DIR}"
-    return text_obs
-
-def get_desc_obs(feasible_action):
-    """
-    Returns additional text description of information when agent has picked up the key or opened the door.
-    Input: observation, feasible_action
-    Output: additional text description
-    """
-    if feasible_action == 'turn left':
-        return 'You have turned left.'
-    elif feasible_action == 'turn right':
-        return 'You have turned right.'
-    elif feasible_action == 'move forward':
-        return 'You have moved forward.' 
-    elif feasible_action == 'pickup key':
-        return 'You have picked up the key.'
-    elif feasible_action == 'open door':
-        return 'You have opened the door.'
-     
-    else:
-        return ''
-    
-def get_llm_policy(env, llm_model, llm_modulo, conv, init_prompt, obs='', to_print=True, grid_text=False, give_add_text_desc=True, give_feasible_actions=True, give_tried_actions=True, save_dir=None):
+def get_llm_policy(env, llm_model, llm_modulo, env_prompter, conv, obs='', to_print=True, grid_text=False, give_add_text_desc=True, give_feasible_actions=True, give_tried_actions=True, save_dir=None, num_agent_steps=30, num_backprompt_steps=10):
     if to_print:
         if grid_text:
-            print(convert_obs_to_grid_text(obs))
+            print(env_prompter.convert_obs_to_grid_text(obs))
         else:
-            print(convert_obs_to_text(obs))
+            print(env_prompter.convert_obs_to_text(obs))
         
     llm_actions = []
     all_actions = []
     all_actions_formatted = []
     add_text_desc = ''
-    for i in range(NUM_AGENT_STEPS):
+    for i in range(num_agent_steps):
         if llm_model != "None":
             FEASIBLE = False
             tried_actions = []
-            for j in range(10):
+            for j in range(num_backprompt_steps):
                 if len(tried_actions) == 0: # move to next step
-                    prompt = get_step_prompt(obs, add_text_desc)
+                    prompt = env_prompter.get_step_prompt(obs, add_text_desc)
                     print('-----------------')
                     print('[STEP PROMPTING]---->')
                 else:
-                    prompt = get_prompt_with_backprompt(obs, backprompt, tried_actions, give_tried_actions=give_tried_actions)
+                    prompt = env_prompter.get_prompt_with_backprompt(obs, backprompt, tried_actions, give_tried_actions=give_tried_actions)
                     print('------[BACK PROMPTING]---->')
                 
                 print(prompt)
@@ -257,8 +108,8 @@ if __name__ == "__main__":
     conv = Conversation(args.llm_model)
     obs, _ = env.reset(seed=args.seed)
     llm_modulo = LLM_Modulo(args.env, seed=args.seed)
-    init_prompt = get_initial_prompt(obs) # not using this for now (step prompt is used instead with TASK_DESC)
-    total_reward = get_llm_policy(env=env, llm_model=args.llm_model, llm_modulo=llm_modulo, conv=conv, init_prompt=init_prompt, obs=obs, to_print=False, grid_text=True, give_add_text_desc=args.add_text_desc, give_feasible_actions=args.give_feasible_actions, give_tried_actions=args.give_tried_actions, save_dir=save_dir)
+    env_prompter = MinigridPromptConstructor(args.env, seed=args.seed)
+    total_reward = get_llm_policy(env=env, llm_model=args.llm_model, llm_modulo=llm_modulo, env_prompter=env_prompter, conv=conv, obs=obs, to_print=False, grid_text=True, give_add_text_desc=args.add_text_desc, give_feasible_actions=args.give_feasible_actions, give_tried_actions=args.give_tried_actions, save_dir=save_dir, num_agent_steps=args.num_agent_steps, num_backprompt_steps=args.num_backprompt_steps)
     
     print('-----------------')
     print(f"Total reward: {total_reward}")

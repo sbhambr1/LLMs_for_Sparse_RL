9a8d1719ffad8dbf3e56b344d3fcaa938f85a2c1
Siddhant <sbhambr1@asu.edu>
2024-04-30 16:30:13 -0700

    baseline expts run

--- scripts/run_baseline_rl_training.sh
+++ scripts/run_baseline_rl_training.sh
@@ -2,16 +2,15 @@
 # MiniGrid-DoorKey-5x5-v0, 
 # MiniGrid-Empty-Random-5x5-v0, 
 # MiniGrid-LavaGapS5-v0, 
-# MiniGrid-KeyCorridorS3R1-v0,
 # MiniGrid-DoorKey-6x6-v0
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
-for env_cfg_seed in 0 2 3 5; do
+for env_cfg_seed in 0; do
     for expt_seed in 1 2 3 4 5 6 7 8 9 10; do
         python scripts/train.py \
             --algo "ppo" \
-            --env "MiniGrid-DoorKey-5x5-v0" \
+            --env "MiniGrid-LavaGapS5-v0" \
             --env_config_seed $env_cfg_seed \
             --seed $expt_seed \
             --save-interval 10 \

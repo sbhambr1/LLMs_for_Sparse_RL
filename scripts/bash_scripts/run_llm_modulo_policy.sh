c8e776faf4115ab617e6ddcdd30e82891826edc7
Amrita Bhattacharjee <amrita.bhatta.97@gmail.com>
2024-05-05 18:56:41 -0700

    more results

--- scripts/bash_scripts/run_llm_modulo_policy.sh
+++ scripts/bash_scripts/run_llm_modulo_policy.sh
@@ -1,24 +1,23 @@
 # env = 
-# MiniGrid-DoorKey-5x5-v0, 
-# MiniGrid-Empty-Random-5x5-v0, 
-# MiniGrid-LavaGapS5-v0, 
-# MiniGrid-KeyCorridorS3R1-v0,
-# MiniGrid-DoorKey-6x6-v0
+# MiniGrid-DoorKey-5x5-v0, seeds= 0 2 3 5
+# MiniGrid-Empty-Random-5x5-v0, seeds= 0 1 2 
+# MiniGrid-LavaGapS5-v0, seeds= 0
+# MiniGrid-DoorKey-6x6-v0, seeds= 0
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
-for seed in 1; do
-    for variation in 1 2 3 4 5 6 7 8 9 10; do
-        python scripts/llm_modulo_policy.py \
+for seed in 0; do
+    for variation in 0 1 2 3; do
+        python3 scripts/llm_modulo_policy.py \
             --env "MiniGrid-LavaGapS5-v0" \
             --seed $seed \
             --variation $variation \
-            --llm-model "gpt-3.5-turbo" \
+            --llm-model "gpt-4-turbo" \
             --add_text_desc "True" \
             --give_feasible_actions "True" \
             --give_tried_actions "True" \
             --additional_expt_info "" \
-            --num_agent_steps 50 \
-            --num_backprompt_steps 20
+            --num_agent_steps 30 \
+            --num_backprompt_steps 10
     done
 done

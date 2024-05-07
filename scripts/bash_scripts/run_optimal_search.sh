3b3b4d2b2f8e87a78c70a83d1558c4621a187351
Siddhant <sbhambr1@asu.edu>
2024-05-07 15:16:09 -0700

    todo: fix a star for doorkey, not using pickup and opendoor

---
+++ scripts/bash_scripts/run_optimal_search.sh
@@ -0,0 +1,8 @@
+# env = 
+# MiniGrid-DoorKey-5x5-v0, seeds= 0
+# MiniGrid-Empty-Random-5x5-v0, seeds= 0
+# MiniGrid-LavaGapS5-v0, seeds= 0
+# MiniGrid-DoorKey-6x6-v0, seeds= 0
+
+python3 scripts/a_star_search.py \
+    --env "MiniGrid-DoorKey-5x5-v0"

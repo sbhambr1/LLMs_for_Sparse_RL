211cf298286898d8dd00da34af5c3ecb91ddb285
Siddhant <sbhambr1@asu.edu>
2024-09-06 14:06:29 -0700

    all llm-modulo expts with pbrs done

--- scripts/bash_scripts/run_reward_shaped_rl_training.sh
+++ scripts/bash_scripts/run_reward_shaped_rl_training.sh
@@ -1,17 +1,25 @@
+# env = 
+# MiniGrid-DoorKey-5x5-v0, seeds= 0 2 3 5
+# MiniGrid-Empty-Random-5x5-v0, seeds= 0 1 2 
+# MiniGrid-LavaGapS5-v0, seeds= 0
+# MiniGrid-DoorKey-6x6-v0, seeds= 0
+
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
-for env_cfg_seed in 2 3 5; do
+for env_cfg_seed in 0 2 3 5; do
     for expt_seed in 1 2 3 4 5; do
-        python scripts/train.py \
-            --algo "ppo" \
-            --env "MiniGrid-DoorKey-5x5-v0" \
-            --env_config_seed $env_cfg_seed \
-            --seed $expt_seed \
-            --save-interval 10 \
-            --frames 100000 \
-            --stochastic \
-            --llm_rs \
-            --llm_variation 1 \
-            --additional_info "LLMpbrs_nsrss"
+        for llm_var in 1 2 3 4; do
+            python scripts/train.py \
+                --algo "ppo" \
+                --env "MiniGrid-DoorKey-5x5-v0" \
+                --env_config_seed $env_cfg_seed \
+                --seed $expt_seed \
+                --save-interval 10 \
+                --frames 100000 \
+                --stochastic \
+                --llm_rs \
+                --llm_variation $llm_var \
+                --additional_info "LLMpbrs_nsrss"
+        done
     done
 done
--- scripts/bash_scripts/run_store_reward_shaping_policy.sh
+++ scripts/bash_scripts/run_store_reward_shaping_policy.sh
@@ -1,3 +1,9 @@
+# env = 
+# MiniGrid-DoorKey-5x5-v0, seeds= 0 2 3 5
+# MiniGrid-Empty-Random-5x5-v0, seeds= 0 1 2 
+# MiniGrid-LavaGapS5-v0, seeds= 0
+# MiniGrid-DoorKey-6x6-v0, seeds= 0
+
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 for seed in 0 2 3 5; do

4ec2d6705c62085263da46989fb78cdddf447922
Siddhant <sbhambr1@asu.edu>
2024-09-22 11:59:49 -0700

    script for running q learning

--- scripts/run_q_learning_baseline_mario.py
+++ scripts/run_q_learning_baseline_mario.py
@@ -1,4 +1,6 @@
 import os
+import utils
+import argparse
 from utils.env_mario import Env_Mario
 from algorithms.configs.q_mario_config import Q_Baseline_Config
 from algorithms.algos.q_learning import Q_Learning
@@ -6,10 +8,17 @@ from algorithms.utils.experiment_manager import Wandb_Logger
 
 os.environ["WANDB_MODE"] = "online"
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--seed", type=int, default=0,
+                    help="random seed (default: 0)")
+
 def main():
+    
+    args = parser.parse_args()
+    utils.seed(args.seed)
     env = Env_Mario(success_reward=1)
     config = Q_Baseline_Config()
-    logger = Wandb_Logger(entity_name='llm_modulo_sparse_rl' ,proj_name='neurips_24', run_name='MARIO_q_baseline')
+    logger = Wandb_Logger(entity_name='llm_modulo_sparse_rl' ,proj_name='neurips_24', run_name='MARIO_q_learning_baseline')
     agent = Q_Learning(env, config, logger=logger)
     agent.train()
 

4fc56285c245e99e940fc663d85456c08627c140
Siddhant <sbhambr1@asu.edu>
2024-09-22 13:22:59 -0700

    stored all vanilla llm rs policies

--- scripts/bash_scripts/run_store_vanilla_llm_reward_shaping_policy.sh
+++ scripts/bash_scripts/run_store_vanilla_llm_reward_shaping_policy.sh
@@ -1,5 +1,5 @@
 # env = 
-# MiniGrid-DoorKey-5x5-v0, seeds= 0 2 3 5
+# MiniGrid-DoorKey-5x5-v0, seeds= 0 3 5
 # MiniGrid-Empty-Random-5x5-v0, seeds= 0 1 2 
 # MiniGrid-LavaGapS5-v0, seeds= 0
 # MiniGrid-DoorKey-6x6-v0, seeds= 0
@@ -7,13 +7,13 @@
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 for seed in 0; do
-    for variation in 0; do
+    for variation in 1 2 3 4 5; do
         python scripts/store_vanilla_llm_reward_shaping_policy.py \
-            --env "MiniGrid-LavaGapS5-v0" \
+            --env "MiniGrid-DoorKey-6x6-v0" \
             --seed $seed \
             --variation $variation \
             --llm_model "gpt-3.5-turbo" \
-            --same_rewards_same_states False \
+            --same_rewards_same_states True \
             --query_type "entire_path"  
     done
 done

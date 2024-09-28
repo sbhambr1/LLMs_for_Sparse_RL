0e49545bf2d1445c778a453cf1df921285351f04
Siddhant <sbhambr1@asu.edu>
2024-09-28 10:49:45 -0700

    running mario vanilla llm pbrs

--- scripts/bash_scripts/mario_run_pbrs_rl_training.sh
+++ scripts/bash_scripts/mario_run_pbrs_rl_training.sh
@@ -1,14 +1,14 @@
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 for variation in 1; do
-    for expt_seed in 7; do
+    for expt_seed in 5; do
         python3 scripts/run_q_learning_pbrs_mario.py \
             --seed $expt_seed \
             --stochastic \
-            --additional_info "_stochastic_llm_modulo_Variation_${variation}_eps_decay" \
+            --additional_info "_stochastic_llm_vanilla_Variation_${variation}_eps_decay" \
             --reshape_reward True \
             --variation $variation \
-            --llm_model "gpt-4o" \
-            --llm_plan "llm_modulo"
+            --llm_model "gpt-3.5-turbo" \
+            --llm_plan "vanilla"
     done
 done
--- scripts/bash_scripts/run_store_reward_shaping_from_pddl.sh
+++ scripts/bash_scripts/run_store_reward_shaping_from_pddl.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
-for variation in 1 2 3; do
+for variation in 1; do
     python scripts/store_reward_shaping_from_pddl.py \
         --env "Mario-8x11" \
         --variation $variation \
-        --llm_model "gpt-4o" \
-        --llm_plan "llm_modulo"
+        --llm_model "gpt-3.5-turbo" \
+        --llm_plan "vanilla"
 done

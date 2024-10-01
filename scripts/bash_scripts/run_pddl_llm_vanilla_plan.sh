d280b46bc0e930df35da1d0c64f6eaff395706c1
Siddhant <sbhambr1@asu.edu>
2024-10-01 11:08:32 -0700

    llama3 results

--- scripts/bash_scripts/run_pddl_llm_vanilla_plan.sh
+++ scripts/bash_scripts/run_pddl_llm_vanilla_plan.sh
@@ -1,6 +1,7 @@
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
-for llm_model in "gpt-3.5-turbo" "gpt-4" "gpt-4o" "claude-3-haiku-20240307" "llama3-8b-instruct-v1:0"; do
+# for llm_model in "gpt-3.5-turbo" "gpt-4" "gpt-4o" "claude-3-haiku-20240307" "meta.llama3-8b-instruct-v1:0"; do
+for llm_model in "meta.llama3-8b-instruct-v1:0"; do
     for variation in 4 5 6; do
         python3 scripts/pddl_llm_vanilla_plan.py \
             --llm_model $llm_model \
